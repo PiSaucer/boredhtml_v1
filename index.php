847fc17bbf2bd186625b43185d7d9751b65c4a5e
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2019-01-13 19:28:39 -1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,238 @@
+<html>
+  <head>
+    <title>
+      Bored In School Right Now Inc HTML
+      </title>
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+      <h1>
+      Bored In School Right Now Inc HTML
+        </h1>
+        <hr>
+        <div class="Games">
+        <h2>Games</h2>
+        <ul>
+        <li><a href="#">Battle Pirates</a></li>
+        <li><a href="#">*Street Racers</a></li>
+        <li><a href="#">Planet of Cells</a></li>
+        <li><a href="#">Vega Conflict</a></li>
+        <li><a href="#">Forge of Empires</a></li>
+        <li><a href="#">Strike Global</a></li>
+        <li><a href="#">Bonk.io</a></li>
+        <li><a href="#">Happy Wheels</a></li>
+        <ul>
+          <li><a href="#">Demo</a></li>
+          <li><a href="#">Full Game</a></li>
+          <li><a href="#">*Actual Website</a></li>
+          </ul>
+        <li><a href="#">2048</a></li>
+        <li><a href="#">Cookie Clicker</a></li>
+        <li><a href="#">Agar.io</a></li>
+        <li><a href="#">Poptropica</a></li>
+        <li><a href="#">Riddle Schools</a></li>
+        <ul>
+          <li><a href="#">Riddle School - Year 1</a></li>
+          <li><a href="#">Riddle School - Year 2</a></li>
+          <li><a href="#">Riddle School - Year 3</a></li>
+          <li><a href="#">Riddle School - Year 4</a></li>
+          <li><a href="#">Riddle School - Year 5</a></li>
+          <li><a href="#">Riddle Transfer - (Year 6)</a></li>
+          </ul>
+        <li><a href="#">Super Mario Flashes</a></li>
+        <ul>
+          <li><a href="#">Super Mario Flash</a></li>
+          <li><a href="#">Super MArio Flash 2.0</a></li>
+          </ul>
+        <li><a href="#">Animation Creator</a></li>
+        <li><a href="#">n-blox (tetris)</a></li>
+        <li><a href="#">Donkey Kong Flash</a></li>
+        <li><a href="#">Run Trilogy</a></li>
+        <ul>
+          <li><a href="#">Run</a></li>
+          <li><a href="#">Run 2</a></li>
+          <li><a href="#">Run 3</a></li>
+          </ul>
+        <li><a href="#">Truck Loader 4</a></li>
+        <li><a href="#">Battalion: Nemesis</a></li>
+        <li><a href="#">Demons Took My Daughter</a></li>
+        <li><a href="#">Formula Racer</a></li>
+        <li><a href="#">Learn To Fly</a></li>
+        <ul>
+          <li><a href="#">Learn to Fly Idle</a></li>
+          <li><a href="#">Learn to Fly 2</a></li>
+          </ul>
+        <li><a href="#">Mechanical Commando</a></li>
+        <li><a href="#">Mr Runner</a></li>
+        <li><a href="#">Neon Races</a></li>
+        <ul>
+          <li><a href="#">Neon Race</a></li>
+          <li><a href="#">Neon Race 2.0</a></li>
+          </ul>
+        <li><a href="#">Sticky Ninja Academy</a></li>
+        <li><a href="#">The Tickler</a></li>
+        <li><a href="#">Bored Button</a></li>
+        <li><a href="#">Sploder</a></li>
+        <li><a href="#">Higher or Lower</a></li>
+        <li><a href="#">Flash Pacman</a></li>
+        <li><a href="#">Megaman Project X</a></li>
+        <li><a href="#">Ultimate Flash Sonic</a></li>
+        <li><a href="#">Portal: The Flash Version</a></li>
+        <li><a href="#">Bloons TDs</a></li>
+        <ul>
+          <li><a href="#">Bloons TD</a></li>
+          <li><a href="#">Bloons TD 2</a></li>
+          <li><a href="#">Bloons TD 3</a></li>
+          <li><a href="#">Bloons TD 4</a></li>
+          <li><a href="#">Bloons TD 5</a></li>
+          </ul>
+        <li><a href="#">The Legend of Zelda And the Lampshade of No Real Significance</a></li>
+        <li><a href="#">There Is No Game</a></li>
+        <li><a href="#">Galaga</a></li>
+        <li><a href="#">Frogger</a></li>
+        <li><a href="#">Minecraft Flash Edition</a></li>
+        <li><a href="#">Mortal Kombat</a></li>
+        <li><a href="#">Doom</a></li>
+        <li><a href="#">Candy Crush</a></li>
+        <li><a href="#">Windows 93</a></li>
+        <li><a href="#">Papa's Restaurants</a></li>
+        <ul>
+          <li><a href="#">Papa's Cupcakeria</a></li>
+          <li><a href="#">Papa's Donuteria</a></li>
+          <li><a href="#">Papa's Freeseria</a></li>
+          <li><a href="#">Papa's Pizzeria</a></li>
+          </ul>
+        <li><a href="#">Pokemon Flash Edition</a></li>
+        <li><a href="#">*Akinator</a></li>
+        <li><a href="#">Club Penguin Online</a></li>
+        <li><a href="#">T-Rex Game</a></li>
+        <li><a href="#">Super Flash Bros</a></li>
+        <ul>
+          <li><a href="#">Super Smash Bros. Flash</a></li>
+          <li><a href="#">Super Smash Bros 2 Flash</a></li>
+          </ul>
+        <li><a href="#">Oregon Trail</a></li>
+        <li><a href="#">Legend of Zelda Orginial</a></li>
+        <li><a href="#">Pokemon Blue</a></li>
+        <li><a href="#">Pokemon Red</a></li>
+        <li><a href="#">Pokemon Yellow</a></li>
+        <li><a href="#">Pokemon Planet</a></li>
+        <li><a href="#">Super Mario World</a></li>
+        <li><a href="#">Super Mario</a></li>
+        <li><a href="#">Metal Slug</a></li>
+        <li><a href="#">Mii Maker</a></li>
+        <li><a href="#">Totally Useless Websites</a></li>
+        <li><a href="#">The Useless Web</a></li>
+        <li><a href="#">mari0</a></li>
+        <li><a href="#">Line Riders</a></li>
+        <ul>
+          <li><a href="#">Line Rider</a></li>
+          <li><a href="#">Line Rider 2</a></li>
+          </ul>
+        </ul>
+        </div>
+        <hr>
+        <div class="HackedGames">
+          <h2>Hacked Games</h2>
+          <ul>
+            <li><a href="#">Run 3</a></li>
+            <li><a href="#">Portal the Flash Version</a></li>
+            <li><a href="#">Knighttron</a></li>
+            <li><a href="#">Acade Prehacks</a></li>
+            <li><a href="#">Hacked Online Games</a></li>
+            </ul>
+          </div>
+        <hr>
+        <div class="MoreFunGameWebsites">
+          <h2>More Fun Games Websites</h2>
+          <ul>
+            <li><a href="#">Bored In School Right Now Flash Edition</a></li>
+            <li><a href="#">Bored In School Right Now Flash 2</a></li>
+            <li><a href="#">Fun Unblocker Games 1.0</a></li>
+            <li><a href="#">Fun Blocker Games 2.0</a></li>
+            <li><a href="#">Unblock Games For School!!!</a></li>
+            <li><a href="#">Unblock Games Bolich</a></li>
+            <li><a href="#">Unblocked Adventures</a></li>
+            <li><a href="#">.io games</a></li>
+            <li><a href="#">*Y8</a></li>
+            <li><a href="#">Cool Math Games</a></li>
+            <li><a href="#">*Poki</a></li>
+            <li><a href="#">Classic Reload</a></li>
+            <li><a href="#">Unblocked Games 66 at School</a></li>
+            <li><a href="#">Internet Arcade</a></li>
+            <li><a href="#">emulator.online</a></li>
+            </ul>
+            <hr>
+          </div>
+          <div class="NotepadTricks">
+            <h2>Notepad Tricks</h2>
+            <h3>FOR PC ONLY!!!</h3>
+            <ul>
+              <li><a href="#">CD virus</a></li>
+              <li><a href="#">Keyboard Dancing</a></li>
+              <li><a href="#">Matrix Hack</a></li>
+              <li><a href="#">The Talking Computer</a></li>
+              <li><a href="#">Password Generator</a></li>
+              <li><a href="#">Fake Error Message</a></li>
+              <li><a href="#">A Folder lock with a password</a></li>
+              </ul>
+              <hr>
+            </div>
+            <div class="Music">
+              <h2>Music</h2>
+              <ul>
+                <li><a href="#">Wix Site</a></li>
+                <li><a href="#">Pandora</a></li>
+                <li><a href="#">Google Play Music</a></li>
+                </ul>
+                <hr>
+              </div>
+            <div class="HowToBypassYoutube">
+              <h2>How to Bypass Youtube</h2>
+              <ul>
+                <li><a href="#">Method 1</a></li>
+                <li><a href="#">Method 2</a></li>
+                <li><a href="#">Method 3</a></li>
+                <li><a href="#">Method 4</a></li>
+                <li><a href="#">Method 5</a></li>
+                <li><a href="#">Method 6</a></li>
+                <li><a href="#">Method 7</a></li>
+                </ul>
+                <hr>
+              </div>
+            <div class="ChatRoom">
+              <h2>Chat Room</h2>
+              <ul>
+                <li><a href="#">Discord</a></li>
+                <li><a href="#">Chatzy</a></li>
+                <li><a href="#">tlk.io</a></li>
+                <li><a href="#">Rumble Talk</a></li>
+                <li><a href="#">Minnit</a></li>
+                </ul>
+                <hr>
+              </div>
+            <div class="GoogleGroups">
+              <h2>Google Groups</h2>
+              <p>Please Join Google Groups So You Can Easily Contact Me OR Any Other. Or Make A Statment A Event Or Porblem.</p>
+              <a href="#">Join Now!</a>
+                <hr>
+              </div>
+            <div class="Feedback">
+              <h2>Feedback</h2>
+              <ul>
+                <li><a href="#">Google Form</a></li>
+                <li><p>Email:</p><a href="#">boredinschoolrightnowinc@googlegroups.com</a></li>
+                </ul>
+              </div>
+            <footer>
+              <hr>
+              <p>"Stay Bored My Dudes"</p>
+              <p>Email: <a href="#">boredinschoolrightnowinc@googlegroups.com</a></p>
+              <p><bold>I DO NOT OWN THESE GAMES</bold></p>
+              <p>*May Not Work At all schools</p>
+              <hr>
+              </footer>
+      </body>
+  </html>
