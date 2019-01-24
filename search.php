06a902043a7012a2ed9cda206e397207782f00a0
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2019-01-23 21:13:17 -1000

    Add files via upload

--- search.php
+++ search.php
@@ -1,30 +1,5 @@
-$file_to_search = "abc.pdf";
-
-search_file('.',$file_to_search);
-
-
-
-
-function search_file($dir,$file_to_search){
-
-$files = scandir($dir);
-
-foreach($files as $key => $value){
-
-    $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
-
-    if(!is_dir($path)) {
-
-        if($file_to_search == $value){
-            echo "file found<br>";
-            echo $path;
-            break;
-        }
-
-    } else if($value != "." && $value != "..") {
-
-        search_file($path, $file_to_search);
-
-    }  
- } 
+<?php
+foreach (glob("*.html") as $filename) {
+    echo "$filename size " . filesize($filename) . "\n";
 }
+?>
