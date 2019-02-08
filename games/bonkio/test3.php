232a94df9163f9d10ca708ac13067e681e679ab7
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2019-02-07 21:06:14 -1000

    Create test3.php

---
+++ games/bonkio/test3.php
@@ -0,0 +1,6 @@
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=4,0,2,0" width="730" height="500">
+<param name=movie value="bonkio.swf">
+<param name=quality value=high>
+<embed src="bonkio.swf" quality=high pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="730" height="500">
+</embed> 
+</object>
